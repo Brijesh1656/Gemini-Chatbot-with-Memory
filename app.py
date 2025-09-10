989e752913ae7f3cdf7a2ff2bff1a30b31f0405a
Brijesh1656 <brijesh7146@gmail.com>
2025-09-10 10:23:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 
 # Load environment variables
 load_dotenv()
-genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+genai.configure(api_key=st.secrets["AIzaSyC8h_cPkBlQJEtnC-JDDrgXDiENXQSqGoo"])
 
 # Initialize Gemini model
 model = genai.GenerativeModel("gemini-2.5-flash")
